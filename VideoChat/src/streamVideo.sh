5b219b4ed4424ebe1e15ffaae92d1e808603d8d1
Serkan Özel <serkan.ozel@boun.edu.tr>
2019-12-14 16:18:27 +0300

    Make bitrate 5000

--- VideoChat/src/streamVideo.sh
+++ VideoChat/src/streamVideo.sh
@@ -1,3 +1,3 @@
 HOST=$1
 PORT=$2
-gst-launch-1.0 v4l2src ! queue ! videoconvert ! queue ! x264enc bitrate=500 speed-preset=superfast tune=zerolatency  ! queue  ! rtph264pay ! queue ! udpsink host=$HOST port=$PORT 
+gst-launch-1.0 v4l2src ! queue ! videoconvert ! queue ! x264enc bitrate=5000 speed-preset=superfast tune=zerolatency  ! queue  ! rtph264pay ! queue ! udpsink host=$HOST port=$PORT 
