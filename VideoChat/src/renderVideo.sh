503e456bd8e7bc65b97bd8e74cbbebb586267cfb
Serkan Özel <serkan.ozel@boun.edu.tr>
2019-12-17 13:02:12 +0300

    Change bash scripts so that we can kill a single gstreamer process

--- VideoChat/src/renderVideo.sh
+++ VideoChat/src/renderVideo.sh
@@ -1,3 +1,4 @@
 HOST=$1
 PORT=$2
-gst-launch-1.0 udpsrc address=$HOST port=$PORT caps="application/x-rtp, media=(string)video, clock-rate=(int)90000, payload=(int)96, encoding-name=(string)H264" ! queue ! rtph264depay ! queue ! avdec_h264 ! queue ! videoconvert ! queue ! xvimagesink
+gst-launch-1.0 udpsrc address=$HOST port=$PORT caps="application/x-rtp, media=(string)video, clock-rate=(int)90000, payload=(int)96, encoding-name=(string)H264" ! queue ! rtph264depay ! queue ! avdec_h264 ! queue ! videoconvert ! queue ! xvimagesink > /dev/null &
+echo $! # pid of last backgrounded task, gstreamer in this case
