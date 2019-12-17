80cdf73a2bf3591084d271683c9f34b13a77561a
FarukOzderim <farukozderim@gmail.com>
2019-12-17 21:24:15 +0300

    Installation.sh added

---
+++ installation.sh
@@ -0,0 +1,7 @@
+sudo apt install v4l-utils
+
+sudo apt-get install libgstreamer1.0-0 gstreamer1.0-plugins-base gstreamer1.0-plugins-good gstreamer1.0-plugins-bad gstreamer1.0-plugins-ugly gstreamer1.0-libav gstreamer1.0-doc gstreamer1.0-tools gstreamer1.0-x gstreamer1.0-alsa gstreamer1.0-gl gstreamer1.0-gtk3 gstreamer1.0-qt5 gstreamer1.0-pulseaudio
+
+sudo apt install gstreamer1.0-plugins-*
+
+sudo apt-get install libnotify-bin
